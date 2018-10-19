3f8bc76d0b4e7933325384dd5f0e8a547d25b7d1
sagar <sagar@sagar-PC>
2018-10-20 00:46:50 +0530

    First Commit Uploading All files

---
+++ ArrayMax/src/Test.java
@@ -0,0 +1,26 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		int a[]={112,51221,611,3,821,9};
+		int loc=0,temp=0;
+		for(int i=0;i<a.length;i++)
+		{
+			for(int j=i+1;j<a.length;j++)
+			{
+				
+			
+			if(a[i]>a[j])
+			{
+				loc=a[i];
+				a[i]=a[j];
+				a[j]=loc;
+				
+			}
+			
+			}
+		}
+		System.out.println(a[a.length-1]);
+	}
+
+}
