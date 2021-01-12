9a38627395d885b24bb383b1ea4a4f30e0afe720
Sagar Valekar <Sagar Valekar>
2021-01-12 11:56:22 +0530

    test file changed

--- ArrayMax/src/Test.java
+++ ArrayMax/src/Test.java
@@ -2,6 +2,8 @@
 public class Test {
 
 	public static void main(String[] args) {
+                      
+ System.out.println("This is for testing purpose");
 		int a[]={112,51221,611,3,821,9};
 		int loc=0,temp=0;
 		for(int i=0;i<a.length;i++)
